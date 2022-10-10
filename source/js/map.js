00548c7ba30f185205d642bd984ff3a17c6f4a82
Regeace <playmarket_pochta@mail.ru>
2022-10-11 00:09:31 +0300

    Дорабатывает стили

--- source/js/map.js
+++ source/js/map.js
@@ -1,2 +1,1 @@
-document.querySelector('.page-footer__map-static').classList.toggle('page-footer__map-static--nojs');
-document.querySelector('.page-footer__map').classList.toggle('page-footer__map--nojs');
+document.querySelector('.map__static-image').classList.remove('map__static-image--nojs');
